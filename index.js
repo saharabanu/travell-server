c0fb607652234b2eca4bb535e739ea561fe5a8eb
saharabanu <saharabanungn@gmail.com>
2021-10-29 21:33:33 +0600

    first added

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+var cors = require('cors');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jy11d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        
+        
+    const database = client.db("tourism_services");
+    const servicesCollection = database.collection("services");
+    // get api 
+    app.get('/services',async(req,res)=>{
+        const cursor = servicesCollection.find({});
+        const services = await cursor.toArray();
+        res.send(services)
+
+    })
+
+    // post api 
+    app.post('/services',async(req,res)=>{
+        const service = req.body;
+        console.log('hit the post api',service)
+       
+        const result =await servicesCollection.insertOne(service);
+        console.log(result)
+        res.json(result)
+    })
+    }
+    finally{
+        // await client.close();
+    }
+
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Tourisom related server is running')
+})
+
+app.listen(port, () => {
+  console.log('Running tourism related server',port)
+})
